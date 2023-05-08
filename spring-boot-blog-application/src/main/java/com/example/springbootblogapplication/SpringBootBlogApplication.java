c73ebe2d8dacd15df7e455bbeac380c0ae3f80fb
deanOfWalls <deanofwalls@gmail.com>
2023-05-08 16:00:02 -0400

    checkpointing to make sure branch up to date

--- spring-boot-blog-application/src/main/java/com/example/springbootblogapplication/SpringBootBlogApplication.java
+++ spring-boot-blog-application/src/main/java/com/example/springbootblogapplication/SpringBootBlogApplication.java
@@ -6,8 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootBlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBlogApplication.class, args);
+
+
+        // Block the main thread to prevent the application from shutting down
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // Ignore
+            }
+        }
+
+    }
 
 }
